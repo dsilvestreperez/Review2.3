cd0657591e1a41715b1c73bc115989548260d1d7
denzelsilvestre <denzelsilvestre@DENZELSILVE132D>
2025-04-14 19:49:26 -0500

    Add project files.

---
+++ Review2.3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Temperature Converter");
+
+double Celdegree, fDegree;
+
+Console.WriteLine("Enter the temperature:");
+
+fDegree = Convert.ToDouble(Console.ReadLine());
+
+Celdegree = 59 * (fDegree - 32);
+
+Console.WriteLine(+fDegree + " F: is equivalent to:" + Celdegree + " C");
+
